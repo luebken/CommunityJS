5bb3aef5f98d1018c545567a43a61fb423d7c9e2
Matthias Lübken <matthias.luebken@gmail.com>
2011-03-23 17:41:08 +0100

    Refactoring: Moved tree creation in own module

--- server.js
+++ server.js
@@ -6,30 +6,10 @@ app.configure(function(){
     app.use(express.static(__dirname + '/public'));
 });
 
-var groups = {};
-for (var i in data) {
-    var continent = data[i].continent;
-    var country = data[i].country;
-    var state = data[i].state;
-    if(!state) state = 'no-state';
-    if(!groups[continent]) {
-        groups[continent] = {};
-    }
-    if(groups[continent][country] == undefined) {
-        groups[continent][country] = [];
-    }
-    if(groups[continent][country][state] == undefined) {
-        groups[continent][country][state] = [];
-    }
-    var link = '<a href="'+ data[i].link+ '">' + data[i].link + '</a>';
-    groups[continent][country][state].push(data[i].town + ': ' +link);
-    groups[continent][country][state].sort();
-}
-
 app.get('/', function(req, res){
   res.render('index.jade', { 
-      groups: groups
-      });  
+      tree: data.groupstree
+  });  
 });
 app.get('/about', function(req, res){
   res.render('about.jade');  
