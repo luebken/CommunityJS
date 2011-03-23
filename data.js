9b9394ed0c33a6dce4f648faddd118de0b5ff6ec
Matthias Lübken <matthias.luebken@gmail.com>
2011-03-23 14:23:14 +0100

    Basic data

---
+++ data.js
@@ -0,0 +1,4 @@
+module.exports = [
+ {continent:'Africa', country:'South Africa', town:'Cape Town', link: 'http://www.meetup.com/Cape-Town-Javascript'}
+,{continent:'North America', country:'Canada', town:'Toronto', link: 'http://www.meetup.com/torontojs'}
+] 
--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-var express = require('express');
+var express = require('express'),
+    data = require('./data');
 var app = express.createServer();
 
 app.configure(function(){
     app.use(express.static(__dirname + '/public'));
 });
 
+var groups = {};
+for (var i in data) {
+    var continent = data[i].continent;
+    var country = data[i].country;
+    if(!groups[continent]) {
+        groups[continent] = {};
+    }
+    if(!groups[continent][country]) {
+        groups[continent][country] = [];
+    }
+    groups[continent][country].push(data[i].town + ': ' + data[i].link);
+}
+console.log(groups);
+
 app.get('/', function(req, res){
-  res.render('index.jade', { title: 'My Site' });  
+  res.render('index.jade', { 
+      groups: groups
+      });  
 });
 
 console.log('Server started on 3000');
