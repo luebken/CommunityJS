9019930e3c8097112f4fd0088a42579911edbf86
Matthias Luebken <matthias.luebken@gmail.com>
2011-05-04 08:09:58 +0200

    bugfix: countries sorted by name

--- data.js
+++ data.js
@@ -1,20 +1,34 @@
 var data = require('./data.dat');
 var groupstree = {};
+
+var continent = {};
+continent.sortedCountries = function () {
+	var sorted = {},
+		key, keys = [];
+	for (key in this.countries) keys.push(key);
+	keys.sort();
+	for (var i = 0; i < keys.length; i++) {
+		sorted[keys[i]] = this.countries[keys[i]];
+	}
+	return sorted;
+}
+
 for (var i in data) {
-    var continent = data[i].continent;
-    var country = data[i].country;
-    var state = data[i].state;
-    var link = '<a href="'+ data[i].link+ '">' + data[i].link + '</a>';
-    var anchor = '<a name="'+ data[i].id+ '">'+ data[i].town + '</a>';
-    var item = anchor + ': ' + link;
-    
-    if(!state) state = 'no-state';
-    if(!groupstree[continent]) groupstree[continent] = {};
-    if(groupstree[continent][country] == undefined) groupstree[continent][country] = [];
-    if(groupstree[continent][country][state] == undefined) groupstree[continent][country][state] = [];
-    
-    groupstree[continent][country][state].push(item);
-    groupstree[continent][country][state].sort();
+	var continentName = data[i].continent;
+	var country = data[i].country;
+	var state = data[i].state;
+	var link = '<a href="'+ data[i].link+ '">' + data[i].link + '</a>';
+	var anchor = '<a name="'+ data[i].id+ '">'+ data[i].town + '</a>';
+	var item = anchor + ': ' + link;
+	
+	if(!state) state = 'no-state';
+	if(!groupstree[continentName]) 
+		groupstree[continentName] = Object.create(continent, {countries: { value : {} } });
+	if(!groupstree[continentName].countries[country]) 
+		groupstree[continentName].countries[country] = [];
+	if(!groupstree[continentName].countries[country][state]) 
+		groupstree[continentName].countries[country][state] = [];
+	groupstree[continentName].countries[country][state].push(item);
 }
 module.exports.groupstree = groupstree;
 module.exports.rawdata = data;
