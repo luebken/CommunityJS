6402b0539f659438e79e585bf2eecf9be6c5be43
Matthias Luebken <matthias@luebken.com>
2011-06-08 08:49:26 +0200

    added local test user

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 var couchdb = {
-	username: 'dedgaisaltobjemsestindin', 
-	password: 'iGuMLAcHPmWqWxqCo3K13Rb4',
-	url: 'https://communityjs.cloudant.com/groups/_all_docs?include_docs=true'
-	//url: 'http://127.0.0.1:5984/groups/_all_docs?include_docs=true'
+	username: 'test', 
+	password: '',
+	url: 'http://127.0.0.1:5984/groups/_all_docs?include_docs=true'
 }
 module.exports.couchdb = couchdb;
 
