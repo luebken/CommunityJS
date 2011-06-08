eae5ebdb8f57d229ec3e3a876addd3c725b4df1c
Matthias Luebken <matthias@luebken.com>
2011-06-08 21:56:40 +0200

    config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var couchdb = {
-	username: 'test', 
-	password: '',
-	url: 'http://127.0.0.1:5984/groups/_all_docs?include_docs=true'
+	username: 'dedgaisaltobjemsestindin', 
+	password: 'iGuMLAcHPmWqWxqCo3K13Rb4',
+	url: 'https://communityjs.cloudant.com/groups/_all_docs?include_docs=true'
 }
 module.exports.couchdb = couchdb;
 
